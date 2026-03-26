d27fdc3bf1951c8f58399264677580f6d1b7175c
Abdulk077 <Khanabdulwadud3@gmail.com>
2026-03-26 15:24:36 +0530

    Learning video stream on fly

--- src/learn.js
+++ src/learn.js
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
 import { v4 as uuidv4 } from "uuid";
-import { stream, streamText, streamSSE } from "hono/streaming";
+import { stream } from "hono/streaming";
+import { join } from "node:path";
+import {logger} from "hono/logger";
 const app = new Hono();
 //const Videos = [];
-
+// using logger middleware
+app.use(logger());
 app.get("/", (c) => {
   return c.text("Hello, World!");
 });
@@ -82,4 +85,52 @@ app.get("/stream-image", (c) => {
     await stream.pipe(response.body);
   });
 });
+// streaming an image using another method this one is best for the text streaming
+app.get("/streams-image", async (c) => {
+  const response = await fetch("https://media.gettyimages.com/id/1475043801/photo/choice-variation-concept.jpg?s=612x612&w=0&k=20&c=zZJfHTFo6764SW6uVaLmLWhxb9AoFRFj35bLdo6aql0=");
+  
+  if (!response.body) return c.text("No body", 500);
+
+  // We pass along the original content type
+  c.header("Content-Type", response.headers.get("Content-Type") || "image/jpeg");
+
+  return stream(c, async (stream) => {
+    const reader = response.body.getReader();
+
+    while (true) {
+      const { done, value } = await reader.read();
+      
+      if (done) break;
+
+      // 'value' is the current chunk (a Uint8Array)
+      // You can see the size here!
+      console.log(`Chunk Received/Sent: ${value.length} bytes`);
+      c.header("chunk-size", value.length);
+      await stream.write(value);
+    }
+  });
+});
+
+// video streaming using stream pipe from local device
+const videoPath = join(import.meta.dir, "video.mp4");
+app.get("/stream-video", async (c) => {
+  // importing file 
+  const file = Bun.file(videoPath)
+
+  // 1. Check if video exists
+  if (!file) {
+    return c.text("Video not found", 404);
+  }
+  c.header("Content-Type", "video/mp4"); 
+  c.header("Content-Length", file.size.toString());
+  
+
+  return stream(c, async (stream) => {
+    // fetching the video from the local device
+    await stream.pipe(file.stream());
+
+  });
+});
+
+
 export default app;
